f9e0ff6454953117acab388c950ffbe314758c3f
dhruvinmaniar123 <maniardhruvin21@gmail.com>
2021-08-13 10:47:46 +0530

    updating file

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("~/Downloads/terraform-gcp-320716-cc4f20273242.json")
+  #credentials = file("~/Downloads/terraform-gcp-320716-cc4f20273242.json")
 
   project = "terraform-gcp-320716"
   region  = "var.region"
